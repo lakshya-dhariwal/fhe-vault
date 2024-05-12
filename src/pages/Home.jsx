314726e7311246e3c38bddf35d60bc731f9af818
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2024-05-13 04:48:20 +0530

    landing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import CustomLink from '@/components/CustomLink';
+import 'cal-sans';
 
 export default function Home() {
   return (
@@ -8,24 +9,23 @@ export default function Home() {
       <main>
         <section className='bg-dark'>
           <div className='flex flex-col items-center justify-center min-h-screen text-white layout'>
-            <CustomLink href='https://github.com/theodorusclarence/vite-react-tailwind-starter'>
-              <h1>Vite React Tailwind Starter</h1>
-            </CustomLink>
-            <p className='mb-4'>
-              By{' '}
-              <CustomLink href='https://theodorusclarence.com'>
-                Theodorus Clarence
-              </CustomLink>
-            </p>
-
-            <div className='mt-8 text-dark'>
-              <p className='text-[#ffe347]'>JIT is on</p>
+            <div className='max-w-[650px] text-center rounded flex-col flex items-center gap-5 p-5'>
+              <h1 style={{ fontFamily: 'Cal Sans' }}> 🏛️ FHE Vault</h1>
+              <h2
+                className='text-gray-300 text-[25px] '
+                style={{ fontFamily: 'Cal Sans' }}
+              >
+                Enable Multi Factor authentication and secure your files. Store
+                and own your data.
+              </h2>
+              <button className='bg-yellow-400 font-semibold hover:opacity-90 text-gray-600 border-b-[2px] border-r-[2px] rounded p-[5px] border-yellow-500'>
+                Get Started
+              </button>
             </div>
-            <footer className='absolute text-gray-300 bottom-2'>
-              © {new Date().getFullYear()}{' '}
-              <CustomLink href='https://theodorusclarence.com'>
-                Theodorus Clarence
-              </CustomLink>
+
+            <footer className='absolute text-gray-300 font-mono bottom-2'>
+              powered by{' '}
+              <CustomLink href='https://fluf-id.vercel.app'>fluf.id</CustomLink>
             </footer>
           </div>
         </section>
