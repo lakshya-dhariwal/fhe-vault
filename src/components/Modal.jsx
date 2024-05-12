6a236ebfe60d4acfc6baca55c02b0f12439cf3e1
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2024-05-13 04:50:48 +0530

    setup modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+// interface ModalProps {
+//     children: React.ReactNode;
+//     closeModal: () => void;
+//     closeOnOutsideClick?: boolean; //optional
+//     modalId?: string; //id of the div in index.html
+// }
+export const ModalPortal  = ({
+    children,
+    closeModal,
+    closeOnOutsideClick = true,
+    modalId = "modal-portal",
+}) => {
+    const elRef = useRef();
+    if (!elRef.current) {
+        elRef.current = document.createElement("div");
+    }
+
+    const handleOutsideClick = (e) => {
+        //checks if click is on modal root or the children if it's on children closeModal won't run
+        if (e.target === e.currentTarget) return closeModal();
+    };
+
+    useEffect(() => {
+        const elRefCurrent = elRef.current;
+        const modalRoot = document.getElementById(modalId);
+
+        if (elRefCurrent && modalRoot) {
+            modalRoot.appendChild(elRefCurrent);
+        }
+        window.history.pushState({ modalOpen: true }, "");
+
+        if (closeOnOutsideClick) {
+            const modalRoot = document.getElementById(modalId);
+            modalRoot?.addEventListener("click", handleOutsideClick);
+        }
+
+        window.addEventListener("popstate", closeModal);
+
+        return () => {
+            if (elRefCurrent && modalRoot) {
+                modalRoot.removeChild(elRefCurrent);
+            }
+            window.removeEventListener("popstate", closeModal);
+
+            modalRoot?.removeEventListener("click", handleOutsideClick);
+        };
+    }, []);
+
+    return <>{createPortal(children, elRef?.current)}</>;
+};
